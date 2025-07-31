577252257c30e955b29b2294ca4da549a5d85f8b
Furginator <rfurgy@gmail.com>
2025-07-31 10:35:20 -0500

    Multiple typescript bug fixes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import '@testing-library/jest-dom'; // Extends expect
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Narthos title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByRole('heading', { name: /Narthos/i, level: 1 });
+  expect(titleElement).toBeInTheDocument();
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import vitest from 'vitest/config'; // Import Vitest config
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+    globals: true,
+  },
   resolve: {
     alias: {
-      '/src': '/home/rob/khora/narthos/src', // Optional, if custom paths
+      '/src': resolve(__dirname, 'src'),
     },
+    extensions: ['.js', '.ts', '.tsx'],
   },
 });
