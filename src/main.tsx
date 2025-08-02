cc469e570a28157640fe743802a5c7a8aada2f15
Furginator <rfurgy@gmail.com>
2025-08-01 21:11:38 -0500

    Remove redundant main.tsx file.

--- src/main.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import NarthosGUI from './components/NarthosGUI';
-import '/src/App.css'; // Global styles
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<NarthosGUI />);
